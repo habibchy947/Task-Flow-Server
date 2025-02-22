5c995faab29b47706b7247525aeea9489e2be21f
Jihanchy <habibullahalquaderi2005@gmail.com>
2025-02-22 15:05:25 +0600

    create delete route for deleting task

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000
@@ -8,6 +8,8 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(express.json())
 app.use(cors())
+
+
 
 // mongodb
 
@@ -28,21 +30,47 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const userCollection = client.db('TaskManagementDB').collection('users')
+    const taskCollection = client.db('TaskManagementDB').collection('tasks')
 
 
     // create user
     app.post('/user', async (req, res) => {
-        const user = req.body
-        const query = { email: user.email }
-        const isExist = await userCollection.findOne(query)
-        if (isExist) {
-          return res.send({ message: 'user already exist', insertedId: null })
-        }
-        const result = await userCollection.insertOne(user)
-        res.send(result)
-      })
+      const user = req.body
+      const query = { email: user.email }
+      const isExist = await userCollection.findOne(query)
+      if (isExist) {
+        return res.send({ message: 'user already exist', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
 
-    
+
+    // Get tasks by user email
+    app.get('/tasks/:email', async (req, res) => {
+      const { email } = req.params;
+      try {
+        const tasks = await taskCollection.find({ email }).toArray();
+        res.json(tasks);
+      } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch tasks' });
+      }
+    });
+
+    // add task
+    app.post('/tasks', async(req,res)=> {
+      const task = req.body
+      const result = await taskCollection.insertOne(task)
+      res.send(result)
+    })
+
+    app.delete('/tasks/:id', async(req,res)=>{
+      const id = req.params.id
+      console.log(id)
+      const query = {_id : new ObjectId(id)}
+      const result = await taskCollection.deleteOne(query)
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -56,9 +84,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('the task is ongoing')
+  res.send('the task is ongoing')
 })
 
 app.listen(port, () => {
-    console.log(`this server is running on port : ${port}`)
+  console.log(`this server is running on port : ${port}`)
 })
