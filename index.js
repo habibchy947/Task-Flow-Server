b72c785771b1d32fee135592e4ecae172c86a071
Jihanchy <habibullahalquaderi2005@gmail.com>
2025-02-22 19:44:47 +0600

    added vaercel.json

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const userCollection = client.db('TaskManagementDB').collection('users')
     const taskCollection = client.db('TaskManagementDB').collection('tasks')
 
@@ -106,14 +106,13 @@ async function run() {
 
     app.delete('/tasks/:id', async(req,res)=>{
       const id = req.params.id
-      console.log(id)
       const query = {_id : new ObjectId(id)}
       const result = await taskCollection.deleteOne(query)
       res.send(result)
     })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
